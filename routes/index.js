40e8bc4f14ac1b970fe99e7a14fce505b9b93109
Leandro Bezerra <leandrobezerra94@hotmail.com>
2019-11-05 00:29:46 -0300

    Initial commit, nodejs with redis database

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const redis = require('redis');
+const client = redis.createClient();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'TecBd' });
+});
+
+module.exports = router;
